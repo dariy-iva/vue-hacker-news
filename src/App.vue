d4d8e85c007af527e38a5e0ff71fed007da00a99
Daria Ivanova <dashann@mail.ru>
2022-04-12 16:13:20 +0300

    feat/add vendor dir, icon in index.html, Header component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,71 @@
 <template>
   <div id="app">
-    <router-view/>
+    <Header />
+    <router-view />
   </div>
 </template>
 
+<script>
+
+import Header from "./components/Header.vue"
+
+export default {
+  name: 'app',
+  components: {
+    Header
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url(./vendor/normalize.css);
+@import url(./vendor/fonts/Verdana/verdana.css);
+
+@keyframes loading {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
 }
 
+body {
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#app {
+  font-family: "Verdana", Arial, sans-serif;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  -webkit-text-size-adjust: 100%;
+  -ms-text-size-adjust: 100%;
+  -moz-text-size-adjust: 100%;
+  width: 100%;
+  min-width: 320px;
+  max-width: 1280px;
+  min-height: 100vh;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-sizing: border-box;
+  --black: #000;
+  --white: #fff;
+  --gray: #828282;
+  --color-bcg-gray: rgb(246, 246, 239);
+  --color-bcg-orange: rgb(255, 102, 0);
+  background-color: var(--color-bcg-gray);
+}
+
+.link-hover {
+  transition: opacity 0.3s;
+}
+
+.link-hover:hover {
+  opacity: 0.6;
+}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <header class="header">
+    <nav class="header__menu">
+      <a href="/" class="header__logo link-hover">
+        <img src="../assets/logo.svg" alt="логотип" class="header__img" />
+        <h1 class="header__title">Hacker News</h1>
+      </a>
+    </nav>
+  </header>
+</template>
+
+<style>
+.header {
+  width: 100%;
+  margin: 0 auto;
+  padding: 2px;
+  box-sizing: border-box;
+  line-height: 0;
+  background-color: var(--color-bcg-orange);
+  display: flex;
+  align-items: center;
+}
+
+.header__logo {
+  display: flex;
+  text-decoration: none;
+}
+
+.header__title {
+  margin: 0 0 0 4px;
+  color: var(--black);
+  font-size: 15px;
+  line-height: normal;
+  font-weight: 700;
+}
+
+.header__img {
+  width: 20px;
+  height: 20px;
+}
+</style>
